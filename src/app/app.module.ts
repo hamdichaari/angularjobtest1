5a3191588d03b554908eaa390c6c6e3a62e842af
unknown <hamdi_chaari@hotmail.fr>
2023-01-27 11:30:08 +0100

    routing et bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SignupComponent
+    SignupComponent,
+    NavbarComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
